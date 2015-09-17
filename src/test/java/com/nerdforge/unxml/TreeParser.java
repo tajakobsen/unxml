ab57d350fb338c73f86bb4cf177a0de10b10e115
Tom Arild Jakobsen <tom@item.no>
2015-09-17 09:57:03 +0200

    Add gradle code for building with coverage

--- src/test/java/com/nerdforge/unxml/TreeParser.java
+++ src/test/java/com/nerdforge/unxml/TreeParser.java
@@ -1,32 +1,33 @@
 package com.nerdforge.unxml;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.nerdforge.unxml.factory.ParsingFactory;
-import com.nerdforge.unxml.parsers.Parser;
-import org.w3c.dom.Document;
+    import com.fasterxml.jackson.databind.node.ObjectNode;
+    import com.nerdforge.unxml.factory.ParsingFactory;
+    import com.nerdforge.unxml.parsers.Parser;
+    import org.w3c.dom.Document;
 
-class TreeParser {
-    private Parsing parsing = ParsingFactory.getInstance().create();
+    class TreeParser {
+        private Parsing parsing = ParsingFactory.getInstance().create();
 
-    public ObjectNode parseXml(String inputXML){
-        Parsing parsing = ParsingFactory.getInstance().create();
-        Document document = parsing.xml().document(inputXML);
+        /**
+         * This method will parse your XML, and return a Jackson ObjectNode
+         * @param inputXML XML as a string
+         * @return A Jackson Json Object Node
+         */
+        public ObjectNode parseXml(String inputXML){
+            Document document = parsing.xml().document(inputXML);
 
-        Parser<ObjectNode> parser = parsing.obj()
-            .attribute("data", "Root", recursiveParser())
-            .build();
+            Parser<ObjectNode> parser = parsing.obj()
+                .attribute("data", "Root", recursiveParser())
+                .build();
 
-        return parser.apply(document);
+            return parser.apply(document);
+        }
+
+        public Parser<ObjectNode> recursiveParser(){
+            return  parsing.obj()
+                .attribute("text", parsing.simple().nodeNameParser())
+                .attribute("children",
+                    parsing.arr("node()", parsing.with(this::recursiveParser))
+                ).build();
+        }
     }
-
-    public Parser<ObjectNode> recursiveParser(){
-
-        return  parsing.obj()
-            .attribute("text", parsing.simple().nodeNameParser())
-            .attribute("children",
-                parsing.arr("node()",
-                    parsing.with(this::recursiveParser) // recursivly
-                )
-            ).build();
-    }
-}
